dde59fb350836b257a0d797d3847c39e571bd24b
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 18:39:08 -0300

    program that checks if a user is of minimum age to get their license

---
+++ exercicio9_pode_dirigir.js
@@ -0,0 +1,12 @@
+//Faça um código que verifique a idade do usuário para ver se já tem idade
+//mínima para adquirir carta de habilitação.
+
+var nome = "Lucas Silva"
+var idade = 18
+
+if (idade >= 18){
+    console.log("Olá, " + nome + "\nVocê passou no nosso teste e já pode dirigir!")
+}
+else{
+    console.log("Olá, " + nome)
+}
