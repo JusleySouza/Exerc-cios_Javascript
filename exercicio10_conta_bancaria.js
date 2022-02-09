8346dc9b67875a421d6920c0bee3789c18453d06
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 18:40:51 -0300

    program that guides the user according to the bank account balance

---
+++ exercicio10_conta_bancaria.js
@@ -0,0 +1,9 @@
+//Crie um código que oriente o usuário de acordo com o saldo da conta bancária
+
+var saldo = 1.500
+
+if (saldo > 0){
+    console.log("Seu saldo está positivo! Gostaria de fazer um investimento?")
+}else{
+    console.log("Seu saldo está negativo! Gostaria de fazer um empréstimo?")
+}
