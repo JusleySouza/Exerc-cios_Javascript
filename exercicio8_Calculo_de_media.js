04242d029e8f808cf8c0f82d421823433ef4b556
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 18:36:52 -0300

    program to calculate the average of a student

---
+++ exercicio8_Calculo_de_media.js
@@ -0,0 +1,9 @@
+//Calcule a media das notas de um aluno:
+
+let trabalhoDeHistoria = 8.0
+let trabalhoDeMatematica = 7.0
+let trabalhoDeCiencia = 10
+let trabalhoDeGeografia = 9.5
+let media = (trabalhoDeHistoria + trabalhoDeMatematica + trabalhoDeCiencia + trabalhoDeGeografia) / 4
+
+console.log("Media: " + media)
