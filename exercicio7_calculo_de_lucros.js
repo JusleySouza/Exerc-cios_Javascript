dc96dca15ca0386d1482c6b297268cb5cc28cefa
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 18:34:40 -0300

    program to calculate the total profit of a company

---
+++ exercicio7_calculo_de_lucros.js
@@ -0,0 +1,8 @@
+//calcule o lucro total de uma empresa durante 3 meses:
+
+let janeiro = 100
+let fevereiro = 300
+let marco = 90
+let lucroTotal = janeiro + fevereiro + marco
+
+console.log("Lucro Total: " + lucroTotal)
