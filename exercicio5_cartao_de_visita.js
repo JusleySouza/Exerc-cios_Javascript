45a675c832a7d20151090920bed2076bc18568b2
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 18:28:23 -0300

    creation of a code that generates a business card with name, surname and profession of customers

---
+++ exercicio5_cartao_de_visita.js
@@ -0,0 +1,9 @@
+//crie um código que gere cartão de visita com nome, sobrenome e profissão
+//dos clientes
+
+var nome = 'Taylor '
+var sobrenome = 'Alvarenga '
+var profissao = " Programadora"
+var cartaoDeVisita = nome + sobrenome + "-" + profissao
+
+console.log(cartaoDeVisita)
