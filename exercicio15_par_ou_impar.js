9b288b9534276c3dcb8ee1a52616136b59522cd4
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 19:33:30 -0300

    program that checks if the number is even or odd

---
+++ exercicio15_par_ou_impar.js
@@ -0,0 +1,10 @@
+//desenvolver um programa que verifica se o numero é par ou impar
+
+var numeroDaSorte = 235
+
+if (numeroDaSorte % 2 == 0){
+    console.log("PAR")
+}
+else{
+    console.log("IMPAR")
+}
