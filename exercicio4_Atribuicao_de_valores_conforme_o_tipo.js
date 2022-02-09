5ac1738d738f3a0af155f852ac1963b362c85dc4
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 17:34:44 -0300

    Working variables according to the type 2

---
+++ exercicio4_Atribuicao_de_valores_conforme_o_tipo.js
@@ -0,0 +1,8 @@
+//Declare cinco variáveis com a nomenclatura a seguir e atribua a tais, um valor que
+//atenda o tipo de dado indicado:
+
+var nome = "Fabiana"
+var idade = 16
+var altura = 1.85
+var possuiPet = true
+var filmesFavoritos = ["Terror", "Suspense", "Ação", "Aventuras", "Super herói", "Romance"]
