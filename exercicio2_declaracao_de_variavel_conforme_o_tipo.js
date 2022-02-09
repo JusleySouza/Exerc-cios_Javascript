f7345b4f969638e8d6345fdcce30c4560cb76247
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 17:29:44 -0300

    Variable declaration according to type.

---
+++ exercicio2_declaracao_de_variavel_conforme_o_tipo.js
@@ -0,0 +1,7 @@
+//Declare 4 variáveis: nome, sobrenome, numeroDaSorte e idade. Atribua um valor para
+//cada uma delas, conforme o tipo.
+
+var nome = "Raquel"
+var sobrenome = "Fernandes"
+var numeroDaSorte = 13
+var idade = 25
