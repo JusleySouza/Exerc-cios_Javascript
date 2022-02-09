7c6f9c7c93a329133653291650a101f9e23584cf
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 19:07:49 -0300

    program to check whether the user is a foreigner or not.

---
+++ exercicio13_estrangeiro.js
@@ -0,0 +1,12 @@
+//Faça um programa que verifique a variável estrangeiro que guarda um valor
+//booleano. Caso o valor seja verdadeiro solicite o Registro Nacional de Estrangeiros(RNE).
+//Se o valor for falso solicite o Cadastro de Pessoa Física (CPF).
+
+var estrangeiro = true
+
+if(estrangeiro){
+    console.log("Você poderia apresentar seu RNE, por favor?")
+}
+else{
+    console.log("Você poderia apresentar seu CPF, por favor?")
+}
