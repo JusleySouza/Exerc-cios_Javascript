69492b9b3c635284ff73537e70f4d537c553a790
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 18:31:04 -0300

    Program using the main mathematical operations

---
+++ exercicio6_calculos.js
@@ -0,0 +1,14 @@
+//Declare 6 variáveis de memória com os seguintes nomes: numeroA, numeroB, soma,
+//subtracao, multiplicacao e divisao 
+
+var numeroA = 20
+var numeroB = 5
+var soma = numeroA + numeroB
+var subtracao = numeroA - numeroB
+var multiplicacao = numeroA * numeroB
+var divisao = numeroA / numeroB
+
+console.log("Soma: " + soma)
+console.log("Subtração: " + subtracao)
+console.log("Mutiplicação: " + multiplicacao)
+console.log("Divisão: " + divisao)
