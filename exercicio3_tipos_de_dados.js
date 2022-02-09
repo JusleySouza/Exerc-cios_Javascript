ca47e35f711ed02f9bf61d9cfecdf794e00f81bb
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 17:31:45 -0300

    Working variables according to the type

---
+++ exercicio3_tipos_de_dados.js
@@ -0,0 +1,10 @@
+//Escreva no arquivo script as variáveis expostas abaixo substituindo os tipos de dados
+//citados por valores:
+
+// var idade = Number
+// var sobrenome = String
+// var salarioMinimo = Float
+
+var idade = 21
+var sobrenome = "Gonçalves"
+var salarioMinimo = 1200.00
