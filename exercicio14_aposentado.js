19fd3c76c72a2145684a2ebd63c709b2265858ab
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 19:13:06 -0300

    program that indicates whether the user can retire or not.

---
+++ exercicio14_aposentado.js
@@ -0,0 +1,10 @@
+//faça um programa que verifique se uma pessoa pode se aposentar com base na idade.
+
+var idade = 66
+
+if (idade > 65){
+    console.log("Você já pode se aposentar")
+}
+else{
+    console.log("Você ainda não pode se aposentar")
+}
