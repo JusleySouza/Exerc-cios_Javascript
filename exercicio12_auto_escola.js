93c217eaf1fd9227cf57ff29275fdc30f660aea5
Jusley Souza <jusleysouza31@gmail.com>
2022-02-09 18:55:25 -0300

    program that informs the user if he is able to drive or not

---
+++ exercicio12_auto_escola.js
@@ -0,0 +1,11 @@
+//Escreva um programa que diga se o usuário esta apto a dirigir ou não
+
+var nome = "Renan Santos"
+var idade = 15
+
+if (idade >= 18){
+    console.log("Olá, " + nome + "\nVocê está apto a dirigir!")
+}
+else{
+    console.log("Olá, " + nome + "\nOps, você ainda não tem a idade mínima para dirigir!")
+}
