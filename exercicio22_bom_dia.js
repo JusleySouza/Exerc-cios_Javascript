af08715ef49c79306a04a7ab9cbc3023013afeaf
Jusley Souza <jusleysouza31@gmail.com>
2022-02-10 17:58:24 -0300

    putting the statement in the program

--- exercicio22_bom_dia.js
+++ exercicio22_bom_dia.js
@@ -1,3 +1,5 @@
+//crie um programa que imprima bom dia 7 vezes
+
 for(var i = 1; i <= 7; i++){
     console.log("\nBom dia!")
 }
--- exercicio23_loop_com_array.js
+++ exercicio23_loop_com_array.js
@@ -1,3 +1,5 @@
+//crie um programa que imprima todos os nomes dos carros no array
+
 var listaDeCarros = ['Fox', 'Celta', 'Gol', 'Uno', 'Sandero']
 
 for (var i = 0; i < listaDeCarros.length; i++){
