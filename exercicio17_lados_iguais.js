a31cac1d7ae7767c2b5fc78eb1fe31986c866858
Jusley Souza <jusleysouza31@gmail.com>
2022-02-10 17:20:38 -0300

    program that classifies the triangle according to the number of equal sides

---
+++ exercicio17_lados_iguais.js
@@ -0,0 +1,14 @@
+//Escreva um programa que diga de que tipo é o triangulo: Equilátero, Isósceles ou Escaleno
+//sendo que a variavel ladosIguais ja estara definida
+
+var ladosIguais = 2
+
+if(ladosIguais == 2 ){
+    console.log("Isósceles")
+}
+else if(ladosIguais == 3 ){
+    console.log("Equilátero")
+}
+else if(ladosIguais == 0 ){
+    console.log("Escaleno")
+}
