8b6982421a22a2fd38aae646849ba142cb15f037
Jusley Souza <jusleysouza31@gmail.com>
2022-02-11 18:41:28 -0300

    program that creates a business card with name and surname

---
+++ exercicio32_cartao_de_visitas.js
@@ -0,0 +1,12 @@
+//crie um programa que imprima seu nome e sobrenome no console
+
+
+function cartaoDeVisitas(nome, sobrenome){
+    var nome = "Carla"
+    var sobrenome = "Viana"
+
+    console.log(nome + " " + sobrenome)
+
+}
+
+cartaoDeVisitas()
