1f15acd4e993646930741e4e58645ebf2e7c8603
Jusley Souza <jusleysouza31@gmail.com>
2022-02-11 18:26:17 -0300

    program that lists the products of a market

---
+++ exercicio31_mercado.js
@@ -0,0 +1,12 @@
+//crie um programa que exiba a lista de produtos de um supermercado
+
+function listarProdutos(){
+
+    var lista = [ 'Leite', 'tomate', 'Biscoito', 'Tapioca']
+    
+    for(var i = 0; i < lista.length; i++){
+        console.log(lista[i])
+    }
+}
+
+listarProdutos()
