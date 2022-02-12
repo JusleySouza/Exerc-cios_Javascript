fbb0a17c396833f190a6ea111ed25d8dabe6a930
Jusley Souza <jusleysouza31@gmail.com>
2022-02-11 21:41:40 -0300

    program that calculates the amount the customer must pay with new laundry updates

---
+++ exercicio37_Calcula_valor_devido2.js
@@ -0,0 +1,11 @@
+//crie um programa que calcule o valor que o cliente deve pagar a lavanderia Digitallaundry 2
+
+var precoEsperado = calculaValorDevido(10)
+
+function calculaValorDevido(pesoDaRoupaSuja) {
+
+    return pesoDaRoupaSuja * 5 + 10
+
+}
+
+console.log("valor a pagar: " + precoEsperado)
